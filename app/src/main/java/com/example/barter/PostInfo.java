ecd7e25de1f9a4ff9cb92ca069999501822907f5
aorri2 <aorri96@naver.com>
2021-06-03 00:04:00 +0900

    채팅 기능 연동 + 수정기능 변경

--- app/src/main/java/com/example/barter/PostInfo.java
+++ app/src/main/java/com/example/barter/PostInfo.java
@@ -1,23 +1,46 @@
 package com.example.barter;
 
-import android.text.Editable;
-
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-public class PostInfo {
+public class PostInfo implements Serializable {
     private String title;
-    private String content;
+    private ArrayList<String> content;
     private String publisher;
+    private Date createdAt;
+    private String id;
 
-    public PostInfo(String title, String content, String publisher) {
+
+
+    public PostInfo(String title, ArrayList<String> content, String publisher, Date createdAt, String id) {
         this.title = title;
         this.content = content;
         this.publisher = publisher;
+        this.createdAt = createdAt;
+        this.id = id;
+    }
+
+    public PostInfo(String title, ArrayList<String> content, String publisher, Date createdAt) {
+        this.title = title;
+        this.content = content;
+        this.publisher = publisher;
+        this.createdAt = createdAt;
     }
     public String getPublisher() {
         return publisher;
     }
+    public Map<String, Object> getPostInfo(){
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("title",title);
+        docData.put("content",content);
+        docData.put("publisher",publisher);
+        docData.put("createdAt",createdAt);
 
+        return docData;
+    }
     public void setPublisher(String publisher) {
         this.publisher = publisher;
     }
@@ -29,11 +52,27 @@ public class PostInfo {
         this.title = title;
     }
 
-    public String getContent() {
+    public ArrayList<String> getContent() {
         return content;
     }
 
-    public void setContent(String content) {
+    public void setContent(ArrayList<String> content) {
         this.content = content;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
